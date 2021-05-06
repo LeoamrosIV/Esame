1dda76154acc727a22ded38e0a48882340562c30
LeoamrosIV <mrlucke11@gmail.com>
2021-05-06 10:25:41 +0200

    Remove demo code
    After project initialization

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using Esame.Data;
+//using Esame.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,6 @@ namespace Esame
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
